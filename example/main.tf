15f1f0463f31c8c1acc24b9cd55d02756359b041
claesgill <Claes-Jonas.Gill@skatteetaten.no>
2020-09-21 13:00:12 +0200

    Added ansible playbook, upd main.tf and variables.tf

--- example/main.tf
+++ example/main.tf
@@ -9,8 +9,8 @@ module "postgres" {
   postgres_service_name                    = "postgres"
   postgres_container_image                 = "postgres:12-alpine"
   postgres_container_port                  = 5432
-  postgres_admin_user                      = "hive"
-  postgres_admin_password                  = "hive"
+  postgres_admin_user                      = var.username
+  postgres_admin_password                  = var.password
   postgres_database                        = "metastore"
   postgres_container_environment_variables = ["PGDATA=/var/lib/postgresql/data"]
 }
--- example/variables.tf
+++ example/variables.tf
@@ -1,3 +1,13 @@
 variable "nomad_acl" {
   type = bool
 }
+
+variable "username" {
+  type        = string
+  description = "Postgres username"
+}
+
+variable "password" {
+  type        = string
+  description = "Postgres password"
+}
