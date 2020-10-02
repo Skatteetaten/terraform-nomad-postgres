7e1c227cc28b0eae02607d7820a82761a871bbbd
dangernil <71001093+dangernil@users.noreply.github.com>
2020-10-02 12:26:01 +0200

    Update .github/action/create-env.py
    Co-authored-by: Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>

--- .github/action/create-env.py
+++ .github/action/create-env.py
@@ -12,8 +12,6 @@ for key in env_keys:
     if key.startswith("ENVKEY_"):
         out_file += key.split("ENVKEY_")[1] + "=" + os.environ.get(key) + "\n"
 
-with open(str(os.environ.get("GITHUB_WORKSPACE")) + "/" + str(os.environ.get("FILE_NAME")), "w") as text_file:
-    text_file.write(out_file)
 
 with open(filepath, "w") as text_file:
     text_file.write(out_file)
