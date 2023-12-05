7074fbb744057c9b9039860a31bc9336d1342a05
Evan Wies <evan@neomantra.net>
2023-12-05 16:48:57 -0500

    Upgrade to use Nomad Provider v2.0

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     nomad = {
       source  = "hashicorp/nomad"
-      version = "~> 1.4.9"
+      version = "~> 2.0.0"
     }
   }
   required_version = ">= 1.0"
